d132cf9503a9abe2861d2bf1a9d0ac136dbbbfa6
fatalis <fatalis@fatalis.pw>
2026-03-18 11:56:11 -0500

    misc: add math library. unused for now

--- src/pch.h
+++ src/pch.h
@@ -6,12 +6,13 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <dvector.h>
+#include <MinHook.h>
+
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <psapi.h>
 #include <commctrl.h>
-
-#include <MinHook.h>
 
 #include "config.h"
 #include "common.h"
